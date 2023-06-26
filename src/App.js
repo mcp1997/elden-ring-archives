ce602572b0eaf751f56820a6ad629635bf0dec39
Matt Phillips <mattcphillips17@gmail.com>
2023-06-26 12:49:29 -0700

    basic file structure and style setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import titleLogo from './assets/media/eldenring_title.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className='title-main' src={titleLogo} alt="elden ring title logo" />
+      <h1 className='font-face-ers subtitle-main'>archives</h1>
     </div>
   );
 }
